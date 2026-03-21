741d76635db4e8a57002749b180d42dd9f210f41
whatwedo/dde <dde@whatwedo.ch>
2026-03-21 00:56:58 +0000

    update dde formula for v2

--- Formula/dde.rb
+++ Formula/dde.rb
@@ -7,22 +7,22 @@ class Dde < Formula
   on_macos do
     on_arm do
       url "https://packages.dde.sh/bin/dde-darwin-arm64"
-      sha256 "c1739627c773523dbdb82983b769dfb76703c3a5f6a599eb2af71011123cce73"
+      sha256 "c64ebf2ce8188c2336a6c083dacb7d6ebadb96e5b0caf6564c9abec5d9a498c9"
     end
     on_intel do
       url "https://packages.dde.sh/bin/dde-darwin-amd64"
-      sha256 "896085e00aa368268b8ab90d8ef5e236b9a693f62e2138bc04882e16abb06a2f"
+      sha256 "8354c48f2e57113410766216db8408e7fdfd46468a942c81c3f98e3dfc80fe8d"
     end
   end
 
   on_linux do
     on_arm do
       url "https://packages.dde.sh/bin/dde-linux-arm64"
-      sha256 "0f6fb5de37ba0740e19d5ff7e55dc4f7c6ec5366e153b8c61e4a6dbd9fbe9b93"
+      sha256 "9400874e0d814176d3ec7fc213486d730fbff0d01f3eb7ba08fbff0c0b2f7922"
     end
     on_intel do
       url "https://packages.dde.sh/bin/dde-linux-amd64"
-      sha256 "16a3adff25066b35b944bbb92eef297754061a8e0e07815ced00ada3e38cb66a"
+      sha256 "d9980d3543be201fd9f7ed8d3ae043b4e82b8a4ce042d03f91db56ae199fbe33"
     end
   end
 
