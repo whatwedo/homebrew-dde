f842ffeafb61f05e228acb99cbfd9caf3ed0d0ef
whatwedo/dde <dde@whatwedo.ch>
2026-04-14 17:38:27 +0000

    update dde formula for v2.0.0-alpha.1

--- Formula/dde.rb
+++ Formula/dde.rb
@@ -1,34 +1,38 @@
 class Dde < Formula
   desc "Docker Development Environment"
   homepage "https://github.com/whatwedo/dde"
-  version "2"
+  version "2.0.0-alpha.1"
   license "AGPL-3.0-or-later"
 
   on_macos do
     on_arm do
-      url "https://packages.dde.sh/bin/dde-darwin-arm64"
-      sha256 "d06926330321e7d588e68630dc75e3d7268cf0d0da483f3d2852c5e232284d69"
+      url "https://packages.dde.sh/bin/2.0.0-alpha.1/dde-darwin-arm64"
+      sha256 "74ea982ced80bedfde6de5fb287a13579ff69806fbacec25e3539910e748e984"
     end
     on_intel do
-      url "https://packages.dde.sh/bin/dde-darwin-amd64"
-      sha256 "d99f6d677c3bc56b3fa3b22896dc62a7a075d69a75beaad72b93849a9758e9b2"
+      url "https://packages.dde.sh/bin/2.0.0-alpha.1/dde-darwin-amd64"
+      sha256 "575082228865706bf03edc7ae83dfd4cfe9cd55f5ebb92f83dae74598988ab16"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://packages.dde.sh/bin/dde-linux-arm64"
-      sha256 "8a558f3595f888c74add934dea90c55bf8853d3eb19cfebe94bf7486a179ef0a"
+      url "https://packages.dde.sh/bin/2.0.0-alpha.1/dde-linux-arm64"
+      sha256 "10c5659226b5a99a2f191d338b062fc8bbe51c641f66b9d0bfa6eb0b3cbc16f1"
     end
     on_intel do
-      url "https://packages.dde.sh/bin/dde-linux-amd64"
-      sha256 "c356b55043e002f5058331f2a4e6aa3007d68523080407ad4d8f992e2dd6c8ad"
+      url "https://packages.dde.sh/bin/2.0.0-alpha.1/dde-linux-amd64"
+      sha256 "c701e1dffdaa02e9853e57c9628e1b54e8506aff42349816f436af576b0a62ce"
     end
   end
 
   def install
     binary_name = stable.url.split("/").last
     bin.install binary_name => "dde"
+  end
+
+  def post_install
+    system "#{bin}/dde", "system:install", "--no-interaction"
   end
 
   test do
