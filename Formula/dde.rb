26a4ac84d12a4b47463b36df96e6ab9f1a2d66f7
whatwedo/dde <dde@whatwedo.ch>
2026-04-16 15:04:21 +0000

    update dde formula for v2.0.0-alpha.3

--- Formula/dde.rb
+++ Formula/dde.rb
@@ -1,28 +1,28 @@
 class Dde < Formula
   desc "Docker Development Environment"
   homepage "https://github.com/whatwedo/dde"
-  version "2.0.0-alpha.2"
+  version "2.0.0-alpha.3"
   license "AGPL-3.0-or-later"
 
   on_macos do
     on_arm do
-      url "https://packages.dde.sh/homebrew/2.0.0-alpha.2/dde-darwin-arm64"
-      sha256 "082d336c42c1d010bf4d5b6e2aa4da9218463e1c161f3e60a91ce2482be70041"
+      url "https://packages.dde.sh/homebrew/2.0.0-alpha.3/dde-darwin-arm64"
+      sha256 "e8af55dd99efd6f534ae9123313e2c01ddfb4d418c686a1a831c192c49daae97"
     end
     on_intel do
-      url "https://packages.dde.sh/homebrew/2.0.0-alpha.2/dde-darwin-amd64"
-      sha256 "4b34a832df5bec80193857b71102b25455e88e6e24de2831b2d383c3eec7fffc"
+      url "https://packages.dde.sh/homebrew/2.0.0-alpha.3/dde-darwin-amd64"
+      sha256 "d17445c9459489373befd257667cf1484da8e71382dd7e848c454a65f67776d2"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://packages.dde.sh/homebrew/2.0.0-alpha.2/dde-linux-arm64"
-      sha256 "b7e2a3235ba0dbb4b25ff60dcbf3a54056aebfd838e8b4ab04418601e802a921"
+      url "https://packages.dde.sh/homebrew/2.0.0-alpha.3/dde-linux-arm64"
+      sha256 "09201f45328d2f08e52690dfb0a526ae7cbe22abb86e9855debff0e74bdf718e"
     end
     on_intel do
-      url "https://packages.dde.sh/homebrew/2.0.0-alpha.2/dde-linux-amd64"
-      sha256 "e5cae55df0ac4417c26efd2ab68da1be7883ca10e5d41ad6a45a6b93cd285e8a"
+      url "https://packages.dde.sh/homebrew/2.0.0-alpha.3/dde-linux-amd64"
+      sha256 "fc5d341be8e52df3c8ed62d79fd76661c04baf6c57cf20289cadd8beec93e411"
     end
   end
 
@@ -30,8 +30,11 @@ class Dde < Formula
     bin.install version.to_s => "dde"
   end
 
-  def post_install
-    system "#{bin}/dde", "system:install", "--no-interaction"
+  def caveats
+    <<~EOS
+      To finish installing dde, run:
+        dde system:install
+    EOS
   end
 
   test do
